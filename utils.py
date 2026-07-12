d64072773f961a319ea151202e98a81c8299b901
Pro-Tweaker <protweaker@protonmail.com>
2026-07-12 22:25:57 +0200

    improve utils

--- utils.py
+++ utils.py
@@ -2,21 +2,21 @@ import os
 import shutil
 
 def copy_file(source_path, destination_path):
-    if not os.path.exists(destination_path):
-        try:
-            shutil.copy(source_path, destination_path)
-            print(f"File copied from {source_path} to {destination_path}")
-        except shutil.Error as e:
-            print(f"Error while copying file: {e}")
-    else:
+    if os.path.exists(destination_path):
         print(f"Destination file {destination_path} already exists. Copy operation skipped.")
+        return
+    try:
+        shutil.copy(source_path, destination_path)
+        print(f"File copied from {source_path} to {destination_path}")
+    except (shutil.Error, OSError) as e:
+        print(f"Error while copying file: {e}")
 
 def move_file(source_path, destination_path):
-    if not os.path.exists(destination_path):
-        try:
-            shutil.move(source_path, destination_path)
-            print(f"File moved from {source_path} to {destination_path}")
-        except shutil.Error as e:
-            print(f"Error while moving file: {e}")
-    else:
+    if os.path.exists(destination_path):
         print(f"Destination file {destination_path} already exists. Move operation skipped.")
+        return
+    try:
+        shutil.move(source_path, destination_path)
+        print(f"File moved from {source_path} to {destination_path}")
+    except (shutil.Error, OSError) as e:
+        print(f"Error while moving file: {e}")
